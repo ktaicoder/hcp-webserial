9905064db14ec09308e7554ae2501ad970a716af
ktaicoder <ktaicodingblock.dev@gmail.com>
2022-08-29 22:38:59 +0900

    remove unused code

--- src/hw/wiseXboard/index.ts
+++ src/hw/wiseXboard/index.ts
@@ -2,8 +2,6 @@ import { IHw } from '@ktaicoder/hcp-base'
 import { WebSerialDevice } from '../WebSerialDevice'
 import { WiseXboardControl } from './WiseXboardControl'
 
-const DELIMITER = Buffer.from([0x52, 0x58, 0x3d, 0x0, 0x0e])
-
 export const wiseXboard: IHw = {
     hwId: 'wiseXboard',
     hwKind: 'serial',
