1680d426945248da0a5ebb7825d2ce636ab8707d
ktaicoder <ktaicodingblock.dev@gmail.com>
2022-09-06 02:43:35 +0900

    rename hardwares to webSerialHardwares

--- src/index.ts
+++ src/index.ts
@@ -5,4 +5,9 @@ import { IHw } from '@ktaicoder/hcp-base'
 import { wiseXboard } from './hw/wiseXboard'
 import { wiseXboardPremium } from './hw/wiseXboardPremium'
 
-export const hardwares: IHw[] = [wiseXboard, wiseXboardPremium]
+export const webSerialHardwares: IHw[] = [wiseXboard, wiseXboardPremium]
+
+export const webSerialHardwareMap: Record<string, IHw> = webSerialHardwares.reduce((acc, cur) => {
+    acc[cur.hwId] = cur
+    return acc
+}, {})
